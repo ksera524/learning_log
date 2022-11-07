1165581aa4a5184188e2527bc2b24948ab6719ab
ksera524 <ksera631@gmail.com>
2022-11-07 19:03:00 +0900

    Strategy

---
+++ Java/designpattern/untitled/src/main/java/Strategy/RandomStrategy.java
@@ -0,0 +1,21 @@
+package Strategy;
+
+import java.util.Random;
+
+public class RandomStrategy implements Strategy{
+    private Random random;
+
+    public RandomStrategy(int seed){
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public Hand nextHand() {
+        return Hand.getHand(random.nextInt(3));
+    }
+
+    @Override
+    public void study(boolean win) {
+        
+    }
+}
