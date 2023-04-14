091bbac474a43b01212bfe5eac8d8be4b7402f5d
ksera524 <ksera631@gmail.com>
2023-04-14 11:13:38 +0900

    setup

---
+++ TypeScript/next-tutorial-zenn/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Hello World</main>
+    </>
+  );
+};
+
+export default Home;
