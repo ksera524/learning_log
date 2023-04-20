fe0cd9951208738e18c86efbf6dc85f6c766aa2e
ksera524 <ksera631@gmail.com>
2023-04-21 01:16:35 +0900

    環境構築中

---
+++ TypeScript/nextjs-gihyo-book/src/themes/fontsize.ts
@@ -0,0 +1,10 @@
+const fontSize: any = [12, 14, 16, 20, 24, 32];
+
+fontSize.extraSmall = fontSize[0];
+fontSize.small = fontSize[1];
+fontSize.medium = fontSize[2];
+fontSize.large = fontSize[3];
+fontSize.extraLarge = fontSize[4];
+fontSize.extraExtraLarge = fontSize[5];
+
+export default fontSize;
---
+++ TypeScript/nextjs-gihyo-book/src/themes/letterSpacings.ts
@@ -0,0 +1,11 @@
+const letterSpacings: string[] = [
+  '17px',
+  '19px',
+  '22px',
+  '26px',
+  '28px',
+  '37px',
+  '43px',
+];
+
+export default letterSpacings;
