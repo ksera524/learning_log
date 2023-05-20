45f46618c032ee77d86323f1ddf8a9094d31c966
ksera524 <ksera631@gmail.com>
2023-05-20 23:54:36 +0900

    file

--- Rust/rust-in-action/phase2/filebasic/src/main.rs
+++ Rust/rust-in-action/phase2/filebasic/src/main.rs
@@ -1,49 +1,40 @@
-#![allow(unused_variables)]
+//! Simulating files one step at a time.
+
+/// Represents a "file", 
+/// which probably lives on a file system.
 #[derive(Debug)]
-struct File {
-    name:String,
-    data:Vec<u8>,
+pub struct File {
+  name: String,
+  data: Vec<u8>,
 }
 
 impl File {
-    fn new(name:&str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-        }
+  /// New files are assumed to be empty, but a name is required.
+  pub fn new(name: &str) -> File {
+    File {
+      name: String::from(name),
+      data: Vec::new(),
     }
-    fn new_with_data(name:&str, data:&Vec<u8>) -> File {
-        let mut f = File::new(name);
-        f.data = data.clone();
-        f
-    }
-    
-    fn open(f:&mut File) -> bool{
-        true
-    }
-    
-    fn close(f:&File) -> bool{
-        true
-    }
-    fn read(self:&File,save_to:&mut Vec<u8>) -> usize{
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-    
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        read_length
-    }
+  }
+
+  /// Returns the file's length in bytes.
+  pub fn len(&self) -> usize {
+    self.data.len()
+  }
+
+  /// Returns the file's name.
+  pub fn name(&self) -> String {
+    self.name.clone()
+  }
 }
 
 fn main() {
-    let f3_data:Vec<u8> = vec![114,117,115,116,33];
-    let mut f3= File::new_with_data("2.txt", &f3_data);
-    let mut buffer:Vec<u8> = vec![];
+  let f1 = File::new("f1.txt");
 
-    let f3_length = f3.read(&mut buffer);
-    let text = String::from_utf8_lossy(&buffer);
+  let f1_name = f1.name();
+  let f1_length = f1.len();
 
-    println!("{:?}",f3);
-    println!("{} is {} bytes long", &f3.name, f3_length);
-    println!("{}",text);
+  println!("{:?}", f1);
+  println!("{} is {} bytes long", f1_name, f1_length);
 }
+
