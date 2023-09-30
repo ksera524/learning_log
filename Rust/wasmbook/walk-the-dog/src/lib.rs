5c37118e39b8e7009ba7a1d651e933673b220593
ksera524 <ksera631@gmail.com>
2023-09-30 17:34:23 +0900

    2.3

--- Rust/wasmbook/walk-the-dog/src/lib.rs
+++ Rust/wasmbook/walk-the-dog/src/lib.rs
@@ -1,18 +1,18 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
-use wasm_bindgen::JsCast;
 use rand::prelude::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::console;
+use std::rc::Rc;
+use std::sync::Mutex;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
+// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
+    
     let canvas = document
         .get_element_by_id("canvas")
         .unwrap()
@@ -26,16 +26,95 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let image = web_sys::HtmlImageElement::new().unwrap();
-    image.set_src("./static/Idle (1).png");
+    wasm_bindgen_futures::spawn_local(async move{
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(),JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = success_tx.clone();
 
-    sierpinski(&context, [(300.0,0.0),(0.0,600.0),(600.0,600.0)],(0,255,0), 5);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move ||{
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt|opt.take()){
+                success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err|{
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt|opt.take()){
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+
+        image.set_src("Idle (1).png");
+
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            5,
+        );
+    });
 
     Ok(())
 }
 
-fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points:[(f64,f64);3],color:(u8,u8,u8)) {
-    let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
+fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: u8,
+) {
+    draw_triangle(&context, points, color);
+    let depth = depth - 1;
+
+    let [top, left, right] = points;
+    if depth > 0 {
+        let mut rng = thread_rng();
+
+        let next_color = (
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+        );
+        let left_middle = midpoint(top, left);
+        let right_middle = midpoint(top, right);
+        let bottom_middle = midpoint(left, right);
+        sierpinski(
+            &context,
+            [top, left_middle, right_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [left_middle, left, bottom_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            &context,
+            [right_middle, bottom_middle, right],
+            next_color,
+            depth,
+        );
+    }
+}
+
+fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
     context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
 
     let [top, left, right] = points;
@@ -47,33 +126,4 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points:[(f64,f64);
     context.close_path();
     context.stroke();
     context.fill();
-}
-
-fn sierpinski(context:&web_sys::CanvasRenderingContext2d, points:[(f64,f64);3],color:(u8,u8,u8), depth:u32) {
-    draw_triangle(&context, points, color);
-
-    let depth = depth - 1;  
-
-    if depth > 0 {
-        let mut rng = rand::thread_rng();
-
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255)
-        );
-
-        let [top, left, right] = points;
-        let left_mid = midpoint(top, left);
-        let right_mid = midpoint(top, right);
-        let bottom_mid = midpoint(left, right);
-
-        sierpinski(&context, [top, left_mid, right_mid], next_color,depth);
-        sierpinski(&context, [left_mid, left, bottom_mid],next_color,depth);
-        sierpinski(&context, [right_mid, bottom_mid, right] ,next_color,depth);
-    }
-}
-
-fn midpoint(p1:(f64,f64), p2:(f64,f64)) -> (f64,f64) {
-    ((p1.0 + p2.0) / 2.0, (p1.1 + p2.1) / 2.0)
 }
