2c8276c4c446c82e6af77f04a7c8d15b36f6fb0d
ksera524 <ksera631@gmail.com>
2023-11-08 00:04:38 +0900

    2.5

--- Rust/wasmbook/walk-the-dog/src/lib.rs
+++ Rust/wasmbook/walk-the-dog/src/lib.rs
@@ -48,34 +48,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move{
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(),JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = success_tx.clone();
-
-
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        let callback = Closure::once(move ||{
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt|opt.take()){
-                success_tx.send(Ok(()));
-            }
-        });
-
-        let error_callback = Closure::once(move |err|{
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt|opt.take()){
-                error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-
-        image.set_src("Idle (1).png");
-
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-    
-
     let json = fetch_json("rhb.json")
         .await
         .expect("failed to fetch json");
@@ -107,18 +79,33 @@ pub fn main_js() -> Result<(), JsValue> {
 
         success_rx.await;
 
-    let sprite = sheet.frames.get("Run (1).png").expect("failed to get sprite");
-    context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-        &image,
-        sprite.frame.x as f64,
-        sprite.frame.y as f64,
-        sprite.frame.w as f64,
-        sprite.frame.h as f64,
-        300.0,
-        300.0,
-        sprite.frame.w as f64,
-        sprite.frame.h as f64,
-    );
+        let mut frame = -1;
+
+        let interval_callback = Closure::wrap(Box::new(move || {
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png",frame + 1);
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+
+            let sprite = sheet.frames.get(&frame_name).expect("failed to get sprite");
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                300.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
+        }) as Box<dyn FnMut()>);
+
+        window.set_interval_with_callback_and_timeout_and_arguments_0(
+            interval_callback.as_ref().unchecked_ref(),
+            50,
+        );
+        
+        interval_callback.forget();
 
 });
     Ok(())
