1a381b4945888ba8a74cf5f8e9268f27ce456d17
ksera524 <ksera631@gmail.com>
2024-05-17 23:39:11 +0900

    5.2.1

--- Rust/wasmbook/walk-the-dog/src/engine.rs
+++ Rust/wasmbook/walk-the-dog/src/engine.rs
@@ -56,6 +56,12 @@ impl Renderer {
             )
             .expect("Drawing is not supported");
     }
+
+    pub fn draw_entire_image(&self ,image:&HtmlImageElement,position:&Point) {
+        self.context
+            .draw_image_with_html_image_element(image,position.x.into(),position.y.into())
+            .expect("Drawing is not supported");
+    }
 }
 
 pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
@@ -210,4 +216,19 @@ fn prepare_input() -> Result<UnboundedReceiver<KeyPress>> {
     onkeyup.forget();
 
     Ok(keyevent_receiver)
+}
+
+pub struct Image {
+    element: HtmlImageElement,
+    position: Point,
+}
+
+impl Image {
+    pub fn new(element: HtmlImageElement, position: Point) -> Self {
+        Self { element, position }
+    }
+
+    pub fn draw(&self,renderer:&Renderer) {
+        renderer.draw_entire_image(&self.element, &self.position)
+    }
 }
--- Rust/wasmbook/walk-the-dog/src/game.rs
+++ Rust/wasmbook/walk-the-dog/src/game.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{
     browser,
-    engine::{self, Game, KeyState, Rect, Renderer},
+    engine::{self, load_image, Game, Image, KeyState, Point, Rect, Renderer},
 };
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
@@ -186,9 +186,14 @@ impl From<JumpingEndState> for RedHatBoyStateMachine {
     }
 }
 
+pub struct Walk {
+    boy: RedHatBoy,
+    background: Image,
+}
+
 pub enum WalkTheDog {
     Loading,
-    Loaded(RedHatBoy),
+    Loaded(Walk),
 }
 
 impl WalkTheDog {
@@ -203,31 +208,35 @@ impl Game for WalkTheDog {
         match self {
             WalkTheDog::Loading => {
                 let json = browser::fetch_json("rhb.json").await?;
+                let background = load_image("BG.png").await?;
 
                 let rhb = RedHatBoy::new(
                     json.into_serde::<Sheet>()?,
                     engine::load_image("rhb.png").await?,
                 );
 
-                Ok(Box::new(WalkTheDog::Loaded(rhb)))
+                Ok(Box::new(WalkTheDog::Loaded(Walk {
+                    boy: rhb,
+                    background: Image::new(background,Point { x: 0, y: 0 }),
+                })))
             }
             WalkTheDog::Loaded(_) => Err(anyhow!("Game already initialized")),
         }
     }
 
     fn update(&mut self, keystate: &KeyState) {
-        if let WalkTheDog::Loaded(rhb) = self {
+        if let WalkTheDog::Loaded(walk) = self {
             if keystate.is_pressed("ArrowRight") {
-                rhb.run_right();
+                walk.boy.run_right();
             }
             if keystate.is_pressed("ArrowDown") {
-                rhb.slide();
+                walk.boy.slide();
             }
             if keystate.is_pressed("Space") {
-                rhb.jump();
+                walk.boy.jump();
             }
 
-            rhb.update();
+            walk.boy.update();
         }
     }
 
@@ -239,8 +248,9 @@ impl Game for WalkTheDog {
             height: 600.0,
         });
 
-        if let WalkTheDog::Loaded(rhb) = self {
-            rhb.draw(renderer);
+        if let WalkTheDog::Loaded(walk) = self {
+            walk.background.draw(renderer);
+            walk.boy.draw(renderer);
         }
     }
 }
