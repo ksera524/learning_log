049dbc013eb05c45e48b9fdfa12423db05893659
ksera524 <ksera631@gmail.com>
2024-05-20 22:51:01 +0900

    5..5.1

--- Rust/wasmbook/walk-the-dog/src/engine.rs
+++ Rust/wasmbook/walk-the-dog/src/engine.rs
@@ -221,11 +221,26 @@ fn prepare_input() -> Result<UnboundedReceiver<KeyPress>> {
 pub struct Image {
     element: HtmlImageElement,
     position: Point,
+    bounding_box: Rect,
 }
 
 impl Image {
     pub fn new(element: HtmlImageElement, position: Point) -> Self {
-        Self { element, position }
+        let bounding_box = Rect {
+            x:position.x.into(),
+            y:position.y.into(),
+            width: element.width() as f32,
+            height: element.height() as f32,
+        };
+
+        Self { 
+            element, 
+            position,
+            bounding_box,}
+    }
+
+    pub fn bounding_box(&self) -> &Rect {
+        &self.bounding_box
     }
 
     pub fn draw(&self,renderer:&Renderer) {
